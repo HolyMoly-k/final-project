9ca9240adb4b006331e396ac395222e07b644d60
HolyMoly-k <koselektvoj@gmail.com>
2026-09-24 12:34:37 +0300

    burger-menu

--- js/script.js
+++ js/script.js
@@ -1,24 +1,38 @@
 const buttons = document.querySelectorAll('.hero-button-list button')
 const form = document.getElementById('hero-form')
 const rentButton = document.getElementById('hero-rent-button')
-const arrowUp = document.getElementById('arrow-up')
-const linkMore = document.getElementById('customer_link-more')
-const customerWindow = document.getElementById('customer_window')
-const customerWindowClose = document.getElementById('customer_window-close')
-let buttonActive;
+const select = document.getElementById('price');
 
 buttons.forEach((button) => {
     button.addEventListener('click', function() {
         buttons.forEach(btn => btn.classList.remove('hero-buy-button'))
         this.classList.add('hero-buy-button')
-        if (rentButton.classList.contains('hero-buy-button')) {
-            form.classList.add('hero-form-rent')
-        } else {
-            form.classList.remove('hero-form-rent')
+
+        if (rentButton.classList.contains('hero-buy-button')){
+          select.options[0].value = '100-200';
+          select.options[0].text = '$100-$200';
+
+          select.options[1].value = '200-400';
+          select.options[1].text = '$200-$400';
+
+          select.options[2].value = '400+';
+          select.options[2].text = '$400+';
+        }
+        else {
+          select.options[0].value = '500-750';
+          select.options[0].text = '$500-$750';
+
+          select.options[1].value = '750-1200';
+          select.options[1].text = '$1200-$750';
+
+          select.options[2].value = '1200+';
+          select.options[2].text = '$1200+';
         }
     })
 })
+  
 
+const arrowUp = document.getElementById('arrow-up')
 window.addEventListener('scroll', () => {
   if (window.scrollY >= 700) {
     arrowUp.classList.add('arrow-active')
@@ -27,11 +41,24 @@ window.addEventListener('scroll', () => {
   }
 })
 
-linkMore.addEventListener('click', function() {
-  customerWindow.classList.add('customer_window-active')
-  customerWindowClose.classList.add('customer_window-close-active')
-  customerWindowClose.addEventListener('click', function() {
-    customerWindow.classList.remove('customer_window-active')
-    customerWindowClose.classList.remove('customer_window-close-active')
+const burgerMenu = document.getElementById('navigation-burger')
+const navMenu = document.getElementById('navigation-menu')
+const burgerTrigger = document.getElementById('burger-trigger')
+const navList = document.querySelectorAll('#nav-list a')
+
+burgerMenu.addEventListener('click', function(){
+  navMenu.classList.toggle('open')
+  burgerTrigger.classList.toggle('open')
+})
+
+burgerTrigger.addEventListener('click', function(){
+  navMenu.classList.remove('open')
+  burgerTrigger.classList.remove('open')
+})
+
+navList.forEach((a) => {
+  a.addEventListener('click', function() {
+    navMenu.classList.remove('open')
+    burgerTrigger.classList.remove('open')
   })
 })
